b08c41965da33af8bffd2c53f0eefc0e74560a0a
José Miguel Villacorta Arteaga <jvillacorta@ucp.edu.pe>
2023-10-26 21:40:07 -0500

    FIRST LOAD
    LOADING PROJECT

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿// ----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+// ----------------------------------------------------------------------------
+
+namespace AppOwnsData.Controllers
+{
+    using Infraestructura.Reporting;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.PowerBI.Api.Models;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        [HttpGet("{controller}/{action}/{reportId}/{*parameters}")]
+        public IActionResult GetReporteRDL(string reportId, string parameters)
+        {            
+            string[] parameterArray = parameters.Split('/');
+            List<ParametroRDL> parametrosRDL = new List<ParametroRDL>();
+            foreach (string parameter in parameterArray)
+            {
+                string[] pamArray = parameter.Split(':');
+                parametrosRDL.Add(new ParametroRDL(pamArray[0], pamArray[1]));
+            }
+
+            ViewBag.ReportId = reportId;
+
+            return View(parametrosRDL);
+        }
+    }
+}
